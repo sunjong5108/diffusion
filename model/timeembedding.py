80e4789f7aa2dba0a2d8f3ad80e4baf115f5832d
Seonjong Kang <81843626+sunjong5108@users.noreply.github.com>
2024-10-04 00:19:57 +0900

    Update timeembedding.py

--- model/timeembedding.py
+++ model/timeembedding.py
@@ -1,16 +1,13 @@
 import torch.nn as nn
-from layer import conv3x3, dense
+from layer import conv3x3, dense, batchnorm
 
 class time_embedding(nn.Module):
-    def __init__(self, out_ch):
+    def __init__(self):
         super().__init__()
 
-        self.out_ch = out_ch
         self.relu = nn.ReLU()
-        self.batchnorm = nn.BatchNorm2d(num_features=out_ch)
 
-    def forward(self, x_img, x_ts):
-        out_ch = self.out_ch
+    def forward(self, x_img, x_ts, out_ch):
         x_parameter = conv3x3(x_img, out_ch)
         x_parameter = self.relu(x_parameter)
 
@@ -22,7 +19,7 @@ class time_embedding(nn.Module):
 
         x_out = conv3x3(x_parameter, out_ch)
         x_out = x_out + x_parameter
-        x_out = self.batchnorm(x_out)
+        x_out = batchnorm(x_out, out_ch)
         x_out = self.relu(x_out)
 
         return x_out
