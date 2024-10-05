12c8870196f6f7a29cdbefb6537e98cb408e5be6
Seonjong Kang <81843626+sunjong5108@users.noreply.github.com>
2024-10-06 01:28:38 +0900

    Update layer.py

--- model/layer.py
+++ model/layer.py
@@ -10,8 +10,29 @@ def conv3x3(in_feat, out_ch):
     conv_layer = nn.Conv2d(in_channels=ch, out_channels=out_ch, kernel_size=3, stride=1, padding=1).to(in_feat.device)
     return conv_layer(in_feat)
 
+def conv1x1(in_feat, out_ch):
+    if len(in_feat.size()) == 3:
+        ch, _, _ = in_feat.size()
+    else:
+        _, ch, _, _ = in_feat.size()
+
+    conv_layer = nn.Conv2d(in_channels=ch, out_channels=out_ch, kernel_size=1, stride=1).to(in_feat.device)
+    return conv_layer(in_feat)
+
+def deconv(in_feat, out_ch):
+    if len(in_feat.size()) == 3:
+        ch, _, _ = in_feat.size()
+    else:
+        _, ch, _, _ = in_feat.size()
+
+    deconv_layer = nn.ConvTranspose2d(in_channels=ch, out_channels=out_ch, kernel_size=4, stride=2, padding=1).to(in_feat.device)
+    return deconv_layer(in_feat)
+
 def dense(in_feat, out_ch):
-    if len(in_feat.size()) == 2:
+    if len(in_feat.size()) == 1:
+        in_feat = in_feat.view(-1, 1)
+        _, ch = in_feat.size()
+    elif len(in_feat.size()) == 2:
         _, ch = in_feat.size()
     else:
         _, _, ch = in_feat.size()
@@ -21,5 +42,8 @@ def dense(in_feat, out_ch):
     return dense_layer(in_feat)
 
 def batchnorm(in_feat, out_ch):
-    bn = nn.BatchNorm2d(num_features=out_ch)
+    if len(in_feat.size()) == 2:
+        bn = nn.BatchNorm1d(num_features=out_ch).to(in_feat.device)
+    else:
+        bn = nn.BatchNorm2d(num_features=out_ch).to(in_feat.device)
     return bn(in_feat)
